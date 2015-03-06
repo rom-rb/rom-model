218c31f5b276b3c441677ba6a03fbea727a89981
Piotr Solnica <piotr.solnica@gmail.com>
2015-03-06 18:18:39 +0100

    Rename Model::{Params=>Attributes} (I am sorry)
    Since Form#attributes uses this class it felt weird to have it called
    Params and we already use `params` to refer to raw params sent from
    request. So...it made sense to rename and avoid confusion.

---
+++ lib/rom/rails/model/attributes.rb
@@ -0,0 +1,72 @@
+require 'virtus'
+require 'active_model/conversion'
+
+module ROM
+  module Model
+    # Mixin for validatable and coercible parameters
+    #
+    # @example
+    #
+    #   class UserParams
+    #     include ROM::Model::Params
+    #
+    #     attribute :email, String
+    #     attribute :age, Integer
+    #
+    #     validates :email, :age, presence: true
+    #   end
+    #
+    #   user_params = UserParams.new(email: '', age: '18')
+    #
+    #   user_params.email # => ''
+    #   user_params.age # => 18
+    #
+    #   user_params.valid? # => false
+    #   user_params.errors # => #<ActiveModel::Errors:0x007fd2423fadb0 ...>
+    #
+    # @api public
+    module Attributes
+      VirtusModel = Virtus.model(strict: true, required: false)
+
+      def self.included(base)
+        base.class_eval do
+          include VirtusModel
+          include ActiveModel::Conversion
+        end
+        base.extend(ClassMethods)
+      end
+
+      def model_name
+        self.class.model_name
+      end
+
+      module ClassMethods
+        DEFAULT_TIMESTAMPS = [:created_at, :updated_at].freeze
+
+        def [](input)
+          input.is_a?(self) ? input : new(input)
+        end
+
+        def set_model_name(name)
+          class_eval <<-RUBY
+            def self.model_name
+              @model_name ||= ActiveModel::Name.new(self, nil, #{name.inspect})
+            end
+          RUBY
+        end
+
+        def timestamps(*attrs)
+          if attrs.empty?
+            DEFAULT_TIMESTAMPS.each do |t|
+              attribute t, DateTime, default: proc { DateTime.now }
+            end
+          else
+            attrs.each do |attr|
+              attribute attr, DateTime, default: proc { DateTime.now }
+            end
+          end
+        end
+      end
+    end
+  end
+end
